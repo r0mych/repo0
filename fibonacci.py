53657a4e6d7288c5b37f693ef8a469d584cb7353
R0mych <romychdevops@gmail.com>
2021-07-10 21:31:57 +0300

    input check added

--- fibonacci.py
+++ fibonacci.py
@@ -2,5 +2,15 @@ def fibonacci(n):
     if n in (1, 2):
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
-
-print(fibonacci(int(input())))
+def isinteger(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+print("введите целое число:")
+chislo = input()
+if isinteger(chislo) == True:
+    print(fibonacci(int(chislo)))
+else:
+    print("введено не целое число")
